76b746fcffc13ed5c0463e8a7dfb64c120ef323e
gUuUnya <koshi.yasui0201@gmail.com>
2024-08-15 23:48:22 +0900

    fix:シード決め

--- src/info.py
+++ src/info.py
@@ -34,6 +34,7 @@ def define_seed(univList):
                 list.append(pair)
             else:
                 left = 0
+                mid = 0
                 right = len(list)-1
                 while left <= right:
                     mid = (left + right) // 2
@@ -41,18 +42,48 @@ def define_seed(univList):
                         right = mid - 1
                     elif pair.PreviousRankOfBack > list[mid].PreviousRankOfBack:
                         left = mid + 1
-                    else:
-                        while pair.PreviousRankOfFront < list[mid].PreviousRankOfFront:
-                            mid -= 1
-                        while pair.PreviousRankOfFront > list[mid].PreviousRankOfFront:
-                            mid += 1
-                        while pair.TeamRank < list[mid].TeamRank:
-                            mid -= 1
-                        while pair.TeamRank > list[mid].TeamRank:
-                            mid += 1
-                        while pair.SchoolRank < list[mid].SchoolRank:
-                            mid -= 1
-                        while pair.SchoolRank > list[mid].SchoolRank:
-                            mid += 1
-                        list.insert(mid, pair)
+                    elif pair.PreviousRankOfBack == list[mid].PreviousRankOfBack:
+                        break
+                print(list[mid].PreviousRankOfFront)
+                print(pair.PreviousRankOfFront)
+                print(list)
+                print(mid)
+                if pair.PreviousRankOfBack == list[mid].PreviousRankOfBack:
+                    while pair.PreviousRankOfFront < list[mid].PreviousRankOfFront:
+                        mid -= 1
+                        if mid < 0:
+                            mid = 0
+                            break
+                    while pair.PreviousRankOfFront > list[mid].PreviousRankOfFront:
+                        mid += 1
+                        if mid >= len(list):
+                            break
+                    print(mid)
+                    if pair.PreviousRankOfFront == list[mid].PreviousRankOfFront:
+                        if mid < len(list) and mid >= 0:
+                            while pair.TeamRank < list[mid].TeamRank:
+                                mid -= 1
+                                if mid < 0:
+                                    mid = 0
+                                    break
+                            while pair.TeamRank > list[mid].TeamRank:
+                                mid += 1
+                                if mid >= len(list):
+                                    break
+                            if pair.TeamRank == list[mid].TeamRank:
+                                    if mid < len(list) and mid >= 0:
+                                        while pair.SchoolRank < list[mid].SchoolRank:
+                                            mid -= 1
+                                            if mid < 0:
+                                                mid = 0
+                                                break
+                                        while pair.SchoolRank > list[mid].SchoolRank:
+                                            mid += 1
+                                            if mid >= len(list):
+                                                break
+                elif pair.PreviousRankOfBack > list[mid].PreviousRankOfBack:
+                    mid += 1
+                print("mid")
+                print(mid)
+                list.insert(mid, pair)
     return list
